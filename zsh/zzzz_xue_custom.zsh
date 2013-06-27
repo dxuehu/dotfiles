6b02f7ff026b7ac60a888fb8344b13ca504842a1
Xue Hu <dxuehu@gmail.com>
2013-06-27 13:11:25 -0700

    oops, added custom zsh powerline stuff

---
+++ zsh/zzzz_xue_custom.zsh
@@ -0,0 +1,6 @@
+#Davids
+#Custom things should really go in ~/.zsh.after/ or before, but I wanted to include it in the repo
+. /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh 
+PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+export EDITOR=/usr/local/bin/vim
+PATH="/usr/local/bin:$PATH"
